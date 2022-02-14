e33e4363567c9f4213a204f722c1c44eb5ba97ba
Nikita Seleznev <92264917+Taho74@users.noreply.github.com>
2022-02-14 08:17:56 +0300

    Add new file

---
+++ Objects.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.qa.java2.lesson1.Practice5;
+
+public class Objects {
+    public static void main(String[] args) {
+        Objects obj1 = new Objects();
+        obj1 = null;
+        Objects obj2 = new Objects();
+        obj2 = null;
+        Objects obj3 = new Objects();
+        obj3 = null;
+        Objects obj4 = new Objects();
+        obj4 = null;
+        Objects obj5 = new Objects();
+        obj5 = null;
+        Objects obj6 = new Objects();
+        obj5 = null;
+        Objects obj7 = new Objects();
+        Objects obj8 = new Objects();
+    }
+}
