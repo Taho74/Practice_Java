f93926393c422f79caec8016127229652b02fde7
Nikita Seleznev <92264917+Taho74@users.noreply.github.com>
2022-02-24 08:55:21 +0300

    Add new file

---
+++ Testswitch.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.qa.java2.lesson1.Practice8;
+
+
+public class Testswitch {
+
+    static void testSw (int a) {
+        switch (a) {
+            case 1:
+                System.out.println("31 день");
+                break;
+            case 2:
+                System.out.println("28 дней");
+                break;
+            case 3:
+                System.out.println("30 дней");
+                break;
+            case 4:
+                System.out.println("31 день");
+                break;
+            case 5:
+                System.out.println("31 день");
+                break;
+            case 6:
+                System.out.println("30 дней");
+                break;
+            case 7:
+                System.out.println("31 день");
+                break;
+            case 8:
+                System.out.println("30 дней");
+                break;
+            case 9:
+                System.out.println("31 день");
+                break;
+            case 10:
+                System.out.println("30 дней");
+                break;
+            case 11:
+                System.out.println("30 дней");
+                break;
+            case 12:
+                System.out.println("31 день");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        testSw(12);
+    }
+}
